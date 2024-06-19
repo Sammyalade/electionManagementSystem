213b62e5c28dd545ef6e984f9f1a9d4b7318964c
Sammyalade <olufemialadeyelu@gmail.com>
2024-06-19 11:43:16 +0100

    election management

---
+++ src/test/java/com/electionManagementSystem/ElectionManagementServiceTest.java
@@ -0,0 +1,13 @@
+package com.electionManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionManagementServiceTest {
+
+    @Test
+    void contextLoads(){
+
+    }
+}
