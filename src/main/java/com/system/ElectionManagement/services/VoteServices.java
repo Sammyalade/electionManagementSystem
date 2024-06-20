54316106c0fa0d2f41bdedab9ec416e1b9fd0fa2
michael <dikandumichael@gmail.com>
2024-06-20 17:53:09 +0100

    Commit from michael

--- src/main/java/com/system/ElectionManagement/services/VoteServices.java
+++ src/main/java/com/system/ElectionManagement/services/VoteServices.java
@@ -6,7 +6,9 @@ import com.system.ElectionManagement.dtos.responses.AddVoteResponse;
 import com.system.ElectionManagement.dtos.responses.VoteResponse;
 import com.system.ElectionManagement.exceptions.VoterNotFoundException;
 import com.system.ElectionManagement.models.Vote;
+import com.system.ElectionManagement.models.Voter;
 import com.system.ElectionManagement.repositories.VoteRepository;
+import com.system.ElectionManagement.repositories.VoterRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,7 @@ public class VoteServices implements VoteServiceImpl {
 
     private final VoteRepository voteRepository;
     private final ModelMapper modelMapper;
+    private final VoterRepository voterRepository;
 
     @Override
     public List<VoteResponse> getAllVote() {
@@ -46,7 +49,10 @@ public class VoteServices implements VoteServiceImpl {
 
     @Override
     public AddVoteResponse addVote(AddVoteRequest voteRequest) {
-        voteRepository.findById(voteRequest.getId());
+            var voter = voterRepository.findById(voteRequest.getId());
+            if(voter.isEmpty()){
+                throw new VoterNotFoundException("No voter found");
+            }
             Vote voteEntity = modelMapper.map(voteRequest, Vote.class);
             voteEntity.setId(voteRequest.getId());
             voteEntity.setElection(voteRequest.getElection());
