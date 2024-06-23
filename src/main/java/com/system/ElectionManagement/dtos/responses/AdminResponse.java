54dea699a4f475beeeabeccb2cf33d44a6495f2d
michael <dikandumichael@gmail.com>
2024-06-23 21:10:25 +0100

    Commit by michael

--- src/main/java/com/system/ElectionManagement/dtos/responses/AdminResponse.java
+++ src/main/java/com/system/ElectionManagement/dtos/responses/AdminResponse.java
@@ -1,5 +1,6 @@
 package com.system.ElectionManagement.dtos.responses;
 
+import com.system.ElectionManagement.models.AdminPrivilege;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,9 @@ import lombok.Setter;
 @Getter
 public class AdminResponse {
     private long id;
+    private String firstName;
+    private String lastName;
+    private AdminPrivilege privilege;
     private String username;
     private String password;
 }
